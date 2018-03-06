227cb51de43d0610046d4468ac8520fa720895b3
Lydia Young <lydiaryoung@gmail.com>
2018-03-06 14:05:19 -0700

    client model set

--- models/client.js
+++ models/client.js
@@ -1,16 +1,46 @@
 module.exports = function(sequelize, DataTypes) {
-  var Author = sequelize.define("Author", {
-    // Giving the Author model a name of type STRING
-    name: DataTypes.STRING
+  var Client = sequelize.define("Client", {
+    // Client entered name (can be full name, user-name...)
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        {allowNull: false}
+      }
+    }
+    // Client's email
+    // isEmail valiation checks for email format
+    email: {
+      type: DataTypes.STRING, 
+      validate:
+        {isEmail: true,
+        allowNull: false}
+    }
+    // Client's password
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        allowNullNull: false}
+      }
+    }, {
+      // Encrypts password ???
+      instanceMethods: {
+        generateHash: function (password) {
+          return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+        },
+        validPassword: function (password) {
+          return bcrypt.compareSync(password, this.password)
+        }
+      }
+    }
   });
 
-  Author.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
-    Author.hasMany(models.Post, {
+  Client.associate = function(models) {
+    // Associating Client with Posts
+    // When an Client is deleted, also delete any associated Posts
+    Client.hasMany(models.Event, {
       onDelete: "cascade"
     });
   };
 
-  return Author;
+  return Client;
 };
