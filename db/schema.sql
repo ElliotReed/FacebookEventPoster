2b63c5734738c30a74259fbd061c60f22f652284
gypsyelliot <elliot@elliotreed.net>
2018-03-06 13:26:57 -0700

    schema

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE facebook_poster;
+
+USE facebook_poster;
+
+CREATE TABLE event (
+    id
+    title
+    DATE
+    START
+    END
+    DESCRIPTION
+    location
+    public
+    posted
+);
+
+CREATE TABLE client (
+    id 
+    name
+    email
+    password
+);
