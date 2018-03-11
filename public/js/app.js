46499c16d60e6500edda441b5984baecd2452783
gypsyelliot <elliot@elliotreed.net>
2018-03-11 14:26:42 -0600

    integrated the facebook code

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,13 @@
-var eventId = 2;
-document.getElementById('postBtn').addEventListener('click', function() {
-  console.log(eventId);
+// var eventId = 2;
+document.querySelector('.fb-button').addEventListener('click', function() {
+  eventId = $(this).attr("data-id")
+  // console.log(eventId);
   $.ajax({
     method: 'get',
     url: '/api/event/' + eventId,
   }).then(function(eventData) {
     eventData.pageId = '203801840389513';
-    console.log(eventData);
+    // console.log(eventData);
 
     var postText =
       'Hey everybody, we have a new event coming up, ' + eventData.title + '!\n' + 
@@ -15,9 +16,9 @@ document.getElementById('postBtn').addEventListener('click', function() {
       'Location: @' + eventData.location + '\n\n' +
       eventData.description + '\n' +
       (eventData.public ? 'This event is open to the public, come on out!' : 'This is a private event.');
-    console.log(postText);
+    // console.log(postText);
     var postData = { postText: postText, pageId: eventData.pageId };
-    console.log(postData);
+    // console.log(postData);
 
     fbPostIt(postData);
   });
